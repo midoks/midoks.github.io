013619490594794e0fc4e566e18ac527fd629dfd
midoks <midoks@163.com>
2016-04-02 00:38:40 +0800

    测试模版

--- resources/js/m.js
+++ resources/js/m.js
@@ -1,3 +1,113 @@
+
+
+//start parser
+$(function(){
+
+var defaults = {
+	  html:         false,        // Enable HTML tags in source
+	  xhtmlOut:     false,        // Use '/' to close single tags (<br />)
+	  breaks:       false,        // Convert '\n' in paragraphs into <br>
+	  langPrefix:   'language-',  // CSS language prefix for fenced blocks
+	  linkify:      true,         // autoconvert URL-like texts to links
+	  linkTarget:   '',           // set target to open link in
+	  typographer:  true,         // Enable smartypants and other sweet transforms
+
+	  // options below are for demo only
+	  _highlight: true,
+	  _strict: false,
+	  _view: 'html'               // html / src / debug
+};
+
+defaults.highlight = function (str, lang) {
+		if (!defaults._highlight || !window.hljs) { return ''; }
+
+		var hljs = window.hljs;
+		if (lang && hljs.getLanguage(lang)) {
+		  try {
+		    return hljs.highlight(lang, str).value;
+		  } catch (__) {}
+		}
+
+		try {
+		  return hljs.highlightAuto(str).value;
+		} catch (__) {}
+
+		return '';
+};
+
+//var markdownParser = new window.Remarkable('commonmark');
+var markdownParser = new window.Remarkable('full', defaults);
+
+// Beautify output of parser for html content
+markdownParser.renderer.rules.table_open = function () {
+  return '<table class="table table-striped">\n';
+};
+
+//
+// Inject line numbers for sync scroll. Notes:
+//
+// - We track only headings and paragraphs on first level. That's enougth.
+// - Footnotes content causes jumps. Level limit filter it automatically.
+//
+
+markdownParser.renderer.rules.paragraph_open = function (tokens, idx) {
+  var line;
+  if (tokens[idx].lines && tokens[idx].level === 0) {
+    line = tokens[idx].lines[0];
+    return '<p class="line" data-line="' + line + '">';
+  }
+  return '<p>';
+};
+
+markdownParser.renderer.rules.heading_open = function (tokens, idx) {
+  var line;
+  if (tokens[idx].lines && tokens[idx].level === 0) {
+    line = tokens[idx].lines[0];
+    return '<h' + tokens[idx].hLevel + ' class="line" data-line="' + line + '">';
+  }
+  return '<h' + tokens[idx].hLevel + '>';
+};
+
+// This values are default
+var md = new Remarkable({
+  html:         false,        // Enable html tags in source
+  xhtmlOut:     true,        // Use '/' to close single tags (<br />)
+  breaks:       true,        // Convert '\n' in paragraphs into <br>
+  langPrefix:   'language-',  // CSS language prefix for fenced blocks
+  linkify:      true,        // Autoconvert url-like texts to links
+  typographer:  true,        // Enable smartypants and other sweet transforms
+
+  // Highlighter function. Should return escaped html,
+  // or '' if input not changed
+  highlight: function (/*str, , lang*/) { return ''; }
+});
+//
+// console.log(md.render('# Remarkable rulezz!'));
+
+
+
+var content = $("#content").html();
+console.log(content);
+var v = markdownParser.render(content);
+//$("#content").html(v);
+console.log(v);
+
+// $(content).each(function(i){
+//
+// console.log(this);
+//
+// 	var o = $(this).html();
+// 	// if (o != ""){
+// 		 var v = markdownParser.render(o);
+// 		 console.log(v);
+//
+// 		 $(this).html(v);
+// 	// }
+// });
+
+}($));
+//end parser
+
 $(function(){
 
 	start_img_check();
@@ -41,9 +151,9 @@ $(function(){
 });
 
 
-(function($){
-	$('code').each(function(i){
-		var text = $(this).html();
-		$(this).html('<pre>'+text+'</pre>');
-	});
-}($));
+// (function($){
+// 	$('code').each(function(i){
+// 		var text = $(this).html();
+// 		$(this).html('<pre>'+text+'</pre>');
+// 	});
+// }($));
