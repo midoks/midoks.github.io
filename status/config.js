90aabdf0086ac04c69272d31e4af0da82fe8cff6
midoks <midoks@163.com>
2021-11-29 11:43:35 +0800

    Update config.js

--- status/config.js
+++ status/config.js
@@ -17,9 +17,6 @@ window.Config = {
   // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
   ApiKeys: [
     'ur685582-15e43aff0a24d270bb328abb',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
   ],
 
   // 是否显示监测站点的链接
